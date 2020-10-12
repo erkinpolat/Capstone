f71ec09b38eaef5c87b0174f0f5d8053515e7481
erkinpolat <44356468+erkinpolat@users.noreply.github.com>
2020-10-12 22:02:55 +0100

    comment system

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('', lambda request: redirect('blog/', permanent=False))
 ]
 
 
